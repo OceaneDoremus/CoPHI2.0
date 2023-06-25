be6f6c272e5922e977a968891b80ce9caee5580e
OceaneDoremus <oceane.doremus.pro@gmail.com>
2023-06-25 16:54:53 +0200

    test server

---
+++ server.mjs
@@ -0,0 +1,19 @@
+import http from 'http';
+import fs from 'fs';
+
+const server = http.createServer((req, res) => {
+  fs.readFile('./src/public/interface.html', (err, content) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Erreur du serveur');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Serveur en cours d\'exécution sur le port 3000');
+});
+
