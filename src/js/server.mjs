4c809cd091e51e6914c5893701599047aa191bef
OceaneDoremus <oceane.doremus.pro@gmail.com>
2023-06-25 17:05:19 +0200

    test

--- src/js/server.mjs
+++ src/js/server.mjs
@@ -2,7 +2,7 @@ import http from 'http';
 import fs from 'fs';
 
 const server = http.createServer((req, res) => {
-  fs.readFile('/interface.html', (err, content) => {
+  fs.readFile('./public/interface.html', (err, content) => {
     if (err) {
       res.writeHead(500, { 'Content-Type': 'text/plain' });
       res.end('Erreur du serveur');
